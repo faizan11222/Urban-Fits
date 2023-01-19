45c215b014e700f71ecfe199841ac62e743d5c07
Dark Reaper <binarshadsaad@gmail.com>
2023-01-19 13:02:33 +0500

    updated the design

---
+++ pages/_button.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Button (props) {
+  return (
+    <button type={props.type} className={`${props.classes} h-12 bg_btn_gold my-6 py-2 rounded-full text-white transition hover:shadow-xl`}>{props.value}</button>
+  )
+}
