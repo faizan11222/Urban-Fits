92278f5ba82aaf8eda90f0444a9f743cfffd4264
Dark Reaper <binarshadsaad@gmail.com>
2023-01-19 18:20:24 +0500

    made 404 page

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="https://i0.wp.com/urbanfits.store/wp-content/uploads/2022/09/cropped-U-logo1-2.png?fit=32%2C32&amp;ssl=1" sizes="32x32"></link>
+      <link rel="icon" href="https://i0.wp.com/urbanfits.store/wp-content/uploads/2022/09/cropped-U-logo1-2.png?fit=192%2C192&amp;ssl=1" sizes="192x192"></link>
+      <link rel="apple-touch-icon" href="https://i0.wp.com/urbanfits.store/wp-content/uploads/2022/09/cropped-U-logo1-2.png?fit=180%2C180&amp;ssl=1"></link>
       <body>
         <Main />
         <NextScript />
