b16ef9945040201a66a726362e71574a5ef5f162
Dark Reaper <binarshadsaad@gmail.com>
2023-01-19 10:29:20 +0500

    my first commit

---
+++ pages/login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Signing from './_signin&up'
+
+export default function Login() {
+  return (
+    <div>
+      <Signing/>
+    </div>
+  )
+}
---
+++ pages/signup.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Signing from './_signin&up'
+
+export default function Signup() {
+  return (
+    <div>
+      <Signing/ >
+    </div>
+  )
+}
